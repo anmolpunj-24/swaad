7b811fa1188da77d35459bd9675f4ba922b199dc
anmolpunj-24 <anmolpunj24@gmail.com>
2026-08-26 23:54:56 +0530

    user repo created

---
+++ backend/src/repositories/userRepository.js
@@ -0,0 +1,55 @@
+const userModel = require("../models/users");
+const customerModel = require("../models/customers");
+
+const addUserRepo = async (userData) => {
+  const newUser = new userModel(userData);
+  const savedUser = await newUser.save();
+  return savedUser;
+};
+
+const addCustomerRepo = async (customerData) => {
+  const newCustomer = new customerModel(customerData);
+  const savedCustomer = await newCustomer.save();
+  return savedCustomer;
+};
+
+const getAllUsersRepo = async () => {
+  return await userModel.find();
+};
+
+const getOneUserRepo = async (id) => {
+  return await userModel.findById(id);
+};
+
+const updateUserRepo = async (id, userData) => {
+  return await userModel.findByIdAndUpdate(id, userData, {
+    returnDocument: "after",
+    runValidators: true,
+  });
+};
+
+const deleteUserRepo = async (id) => {
+  return await userModel.findByIdAndUpdate(
+    id,
+    { deletedAt: new Date(), isActive: false },
+    { new: true },
+  );
+};
+
+const uploadProfileRepo = async (user, file) => {
+  user.profile = file.filename;
+
+  const updatedUserProfile = await user.save();
+
+  return updatedUserProfile;
+};
+
+module.exports = {
+  getAllUsersRepo,
+  getOneUserRepo,
+  updateUserRepo,
+  deleteUserRepo,
+  uploadProfileRepo,
+  addUserRepo,
+  addCustomerRepo,
+};
--- backend/src/services/userServices.js
+++ backend/src/services/userServices.js
@@ -1,59 +1,43 @@
 const fs = require("fs");
 const path = require("path");
 
-const userModel = require("../models/users");
-const customerModel = require("../models/customers");
+const userRepo = require("../repositories/userRepository");
 
 const addUserService = async (userData) => {
   const { name, email, password, gender, dob, phone } = userData;
 
-  const newUser = new userModel({
-    name, 
-    email,
-    password,
-  });
+  const savedUser = await userRepo.addUserRepo({ name, email, password });
 
-  const savedUser = await newUser.save();
-
-  const newCustomer = new customerModel({
-    userId: savedUser?._id,
+  await userRepo.addCustomerRepo({
+    userId: savedUser._id,
     gender,
     dob,
     phone,
   });
 
-  await newCustomer.save();
-
   return savedUser;
 };
 
 const getAllUsersService = async () => {
-  const allUsers = await userModel.find();
+  const allUsers = await userRepo.getAllUsersRepo();
 
   return allUsers;
 };
 
 const getOneUserService = async (id) => {
-  const userData = await userModel.findById(id);
+  const userData = await userRepo.getOneUserRepo(id);
 
   return userData;
 };
 
 const updateUserService = async (id, userData) => {
-  const updatedUser = await userModel.findByIdAndUpdate(id, userData, {
-    returnDocument: "after",
-    runValidators: true,
-  }); 
+  const updatedUser = await userRepo.updateUserRepo(id, userData);
 
   return updatedUser;
 };
 
 const deleteUserService = async (id) => {
-  const deletedUser = await userModel.findByIdAndUpdate(
-    id,
-    { deletedAt: new Date(), isActive: false },
-    { new: true },
-  );
+  const deletedUser = await userRepo.deleteUserRepo(id);
   return deletedUser;
 };
 
@@ -71,9 +55,7 @@ const uploadProfileService = async (user, file) => {
     }
   }
 
-  user.profile = file.filename;
-
-  const updatedUserProfile = await user.save();
+  const updatedUserProfile = await userRepo.uploadProfileRepo(user, file);
 
   return updatedUserProfile;
 };
